49b40791fc510be29380cc257586f9e163a5117c
Lee <van5150@naver,com>
2013-01-09 01:22:22 +0900

    0109

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyGithub::Application.config.secret_token = 'c25ded6daf24ab556e854990aae4bb44e5ae98f82265ffa733416df591ebaefb174c482b7889967b3d70fb7e5290291ebfcf731ababb8c73d065fc259f0dc584'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MyGithub::Application.config.session_store :cookie_store, key: '_my_github_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MyGithub::Application.config.session_store :active_record_store
